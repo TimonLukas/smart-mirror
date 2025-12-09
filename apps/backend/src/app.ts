6b395c43739805665156b15a72c4162952097339
Timon Lukas <timon.lukas@pentlandfirth.com>
2025-12-09 10:28:38 +0100

    Scaffold shared library

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -1,1 +1,3 @@
-console.log('hello world!')
+import { add } from '@smart-mirror/lib-common/foo'
+
+console.log('1 + 2 =', add(1, 2))
--- apps/frontend/src/main.ts
+++ apps/frontend/src/main.ts
@@ -1,8 +1,11 @@
 import './assets/main.css'
+import { add } from '@smart-mirror/lib-common/foo'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
+
+console.log('1 + 2 =', add(1, 2))
 
 const app = createApp(App)
 
